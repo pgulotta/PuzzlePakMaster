77fadd9d9e740c4d4c4b0c0311d94889991e5e39
pat26 <pgulotta@gmail.com>
2020-09-07 16:10:02 -0500

    initial load

---
+++ controller/imagesfactory.cpp
@@ -0,0 +1,26 @@
+#include "imagesfactory.hpp"
+
+ImagesFactory::ImagesFactory()
+{
+
+}
+
+void ImagesFactory::selectPuzzle( int previousPuzzleIndex )
+{
+
+}
+
+void ImagesFactory::nextPuzzle()
+{
+
+}
+
+int ImagesFactory::currentPuzzleIndex() const
+{
+  return mCurrentPuzzleIndex;
+}
+
+int ImagesFactory::puzzlesCount() const
+{
+  return 0;
+}
---
+++ controller/imagesfactory.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+class ImagesFactory
+{
+public:
+  ImagesFactory();
+  void selectPuzzle( int previousPuzzleIndex );
+  void nextPuzzle();
+  int currentPuzzleIndex() const;
+  int puzzlesCount() const;
+
+private:
+  int mCurrentPuzzleIndex;
+};
+
+
